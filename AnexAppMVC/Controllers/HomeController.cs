f6ecc6df3734e686ec79eb16d18d13f5fa0a0d1c
Gökhan ÖNAL <gokhan1onal@gmail.com>
2022-08-20 11:57:57 +0300

    First Commit
    Anex Tour rezervasyon kayıtlarını Web API üzerinden çeken Asp.NET MVC Web App

---
+++ AnexAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AnexAppMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace AnexAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string W_JSON = "";
+            using (WebClient wc = new WebClient())
+            {
+                W_JSON = wc.DownloadString("http://localhost:63839/api/Reservations");
+            }
+
+            List<ReservationModel> W_Reservations = MyJsonToList<ReservationModel>(W_JSON);
+            return View(W_Reservations);
+        }
+
+
+        /// <summary>
+        /// Add Gökhan ÖNAL
+        /// JSON verisini Object Liste çevirir
+        /// </summary>
+        /// <returns></returns>
+        List<pObject> MyJsonToList<pObject>(string pJson)
+        {
+            try
+            {
+                return (List<pObject>)JsonConvert.DeserializeObject(pJson, typeof(List<pObject>));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodBase.GetCurrentMethod().Name + "_" + ex.Message);
+            }
+        }
+    }
+}
---
+++ AnexAppMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnexAppMVC.Global" Language="C#" %>
